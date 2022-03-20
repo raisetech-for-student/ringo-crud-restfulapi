0c9d49a5d6a5d11b9c4e6d904e6a13bac6e81222
MACBOOK_MAKO <>
2022-03-20 23:26:46 +0900

    myFirstCommit for API-Tutorial

---
+++ src/main/java/com/example/SpringRESTfulTurorials/SpringResTfulTurorialsApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringRESTfulTurorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResTfulTurorialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringResTfulTurorialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringRESTfulTurorials/SpringResTfulTurorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringRESTfulTurorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResTfulTurorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
