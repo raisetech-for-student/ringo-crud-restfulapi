5bc7d2feb4753946a2f639345025773cfc977f16
MACBOOK_MAKO <>
2022-05-23 21:29:19 +0900

    [new]Create project for Spring Boot

---
+++ src/main/java/com/raisetech/RingoResTfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.raisetech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RingoResTfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RingoResTfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raisetech/RingoResTfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raisetech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RingoResTfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
