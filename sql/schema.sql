69bca8742b92f9aeb36a111c92fc0a16b214e2ed
watanabemasao <watanabe.masao0804@gmail.com>
2022-07-30 21:09:39 +0900

    docker-compose.yaml及びmysql関連ファイルの作成

---
+++ sql/schema.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS user;
+/* ユーザーテーブル作成 */
+CREATE TABLE user
+(
+  id CHAR(26) NOT NULL
+  ,name VARCHAR(255)  NOT NULL
+  ,birthdate DATE NOT NULL
+  ,deleted BOOLEAN NOT NULL
+  ,created_at DATETIME NOT NULL
+  ,created_by VARCHAR(255) NOT NULL
+  ,updated_at DATETIME NOT NULL
+  ,updated_by VARCHAR(255) NOT NULL
+  ,deleted_at DATETIME
+  ,deleted_by VARCHAR(255)
+  ,PRIMARY KEY (id)
+);
